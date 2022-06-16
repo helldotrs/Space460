3d7b30d6c5ac845bd43cd1180015560bc99e848d
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-06-16 09:37:20 +0200

    changing missile color to blue
    thank you coolors.co !

--- game.py
+++ game.py
@@ -27,18 +27,19 @@ display.set_caption("Space 460")
 #classes
 class Color():
     def __init__(self):
-        self.WHITE      = (255,  255,    255 )
-        self.BLACK      = (0,    0,      0   )
-        self.RED        = (230,  0,      100 )            #looks dope, must use if game uses colors in future.
-        self.GRAY       = (153,  153,    153 )
-        self.GREEN      = (0,    204,    0   )
+        self.WHITE      = (255,     255,    255 )
+        self.BLACK      = (0,       0,      0   )
+        self.RED        = (230,     0,      100 )            #looks dope, must use if game uses colors in future.
+        self.GRAY       = (153,     153,    153 )
+        self.GREEN      = (0,       204,    0   )
+        self.BLUE       = (117,     182,    243 )
 
         self.bg         = self.BLACK
         self.player     = self.WHITE
         self.bullet     = self.GREEN
         self.star       = self.GRAY
         self.enemy      = self.RED
-        self.missile    = self.GREEN #FIXME add blue and change to blue
+        self.missile    = self.BLUE #FIXME add blue and change to blue
         self.TEXT       = self.RED
 
 color       = Color()
