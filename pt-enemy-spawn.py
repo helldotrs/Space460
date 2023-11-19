cd229023fda50fe8e7ba66d38f1a1979c9219823
Rebekah <macrosystem@proton.me>
2023-11-19 02:23:13 +0100

    Update pt-enemy-spawn.py

--- pt-enemy-spawn.py
+++ pt-enemy-spawn.py
@@ -3,15 +3,33 @@ import time
 enemies = []
 n = 0
 
-white True:
-  time.sleep(1)
+def draw_enemies(enemies):
   for enemy in enemies:
     print(enemy)
   print("/end")
-  
-  if len(enemies) >= 10: 
-    enemies.pop(0)
+
+def pause_game(t=1):
+  time.sleep(t)
+
+"""FIXME
+def limit_list_size(input_list, size=10)
+  if len(input_list) >= 10: 
+    input_list.pop(0)
+    """
+def add_enemy():
   enemies.append(n)
 
   n += 1
+  
+  
+white True:
+  draw_enemies(enemies)
+  pause_game()
+  limit_size(enemies)
+
+  #limit_list_size(enemies)
+  if len(enemies) >= 10: 
+    enemies.pop(0)
+
+  add_enemy()
 
