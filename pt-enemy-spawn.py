1c2f7eb97f144d72c34c48d633a7a17258adb111
Rebekah <macrosystem@proton.me>
2023-11-19 02:12:50 +0100

    Update pt-enemy-spawn.py

--- pt-enemy-spawn.py
+++ pt-enemy-spawn.py
@@ -1,10 +1,17 @@
 import time
 
 enemies = []
+n = 0
 
 white True:
   time.sleep(1)
   for enemy in enemies:
     print(enemy)
   print("/end")
+  
+  if len(enemies) >= 10: 
+    enemies.pop(0)
+  enemies.append(n)
+
+  n += 1
 
