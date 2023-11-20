6321946d608112a665485c548ec5e6ab8805e8d9
Rebekah <macrosystem@proton.me>
2023-11-20 13:42:30 +0100

    Create build03.py

---
+++ build03.py
@@ -0,0 +1,12 @@
+# double hashsign to start a segment
+## import external
+import pygame
+import time
+
+## variables
+game_running = True
+pause_time = 1
+
+## mainloop
+while game_running:
+  time.pause(pause_time) 
