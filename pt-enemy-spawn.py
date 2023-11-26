4e3b723e9cc38cf2f9ce850f041932de64c72246
Rebekah <macrosystem@proton.me>
2023-11-26 15:55:52 +0100

    Update pt-enemy-spawn.py

--- pt-enemy-spawn.py
+++ pt-enemy-spawn.py
@@ -15,11 +15,20 @@ def enemies_add_standard():
     enemies_list.append("standard enemy")
 
 def enemies_limit_size():
-    limit_list_size(enemies_list, enemies_list_size)
+    global enemies_list
+    enemies_list = limit_list_size(enemies_list, enemies_list_size)
 
 def limit_list_size(input_list, input_size):
     while len(input_list) > input_size:
         input_list.pop(0)
+    return input_list
+
+# Example usage
+enemies_add_standard()
+enemies_add_standard()
+enemies_add_standard()
+enemies_draw(enemies_list)
+
 
 
 
