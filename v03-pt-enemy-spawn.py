aac440aff60f361df726a944b00fd3c646b563ad
Rebekah <macrosystem@proton.me>
2023-11-26 16:34:23 +0100

    Update v03-pt-enemy-spawn.py

--- v03-pt-enemy-spawn.py
+++ v03-pt-enemy-spawn.py
@@ -1,29 +1,27 @@
 enemies_list = []
 ENEMIES_LIST_SIZE = 10
 
-def draw_enemies(enemies):
-    """Print the list of enemies."""
-    for enemy in enemies:
-        print(enemy)
+def print_enemies_list():
+    print_list_instances(enemies_list)
+
+def print_list_instances(input_list):
+    for instant in input_list:
+        print(instant)
     print("/end")
 
 def add_enemy(enemy_type="std"):
-    """Add an enemy to the list based on the given type."""
     if enemy_type == "std":
         add_standard_enemy()
     limit_list_size()
 
 def add_standard_enemy():
-    """Add a standard enemy to the list."""
     enemies_list.append("standard enemy")
 
-def limit_list_size():
-    """Limit the size of the enemies list."""
+def trim_enemies_list_size():
     global enemies_list
     enemies_list = trim_list_size(enemies_list, ENEMIES_LIST_SIZE)
 
 def trim_list_size(input_list, size):
-    """Trim the list size to the specified size."""
     while len(input_list) > size:
         input_list.pop(0)
     return input_list
@@ -32,4 +30,4 @@ def trim_list_size(input_list, size):
 add_standard_enemy()
 add_standard_enemy()
 add_standard_enemy()
-draw_enemies(enemies_list)
+print_enemies_l
