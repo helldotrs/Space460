82c0f727dd040395e19bc80a8bff32b2fbf9ea08
Rebekah <macrosystem@proton.me>
2023-11-27 20:23:17 +0100

    Update and rename build03.py to v03_game.py

---
+++ v03_game.py
@@ -0,0 +1,38 @@
+# double hashsign to start a segment
+## import external
+import pygame
+import time
+import v03_pt_enemy_spawn
+
+## variables
+game_running = True
+pause_length = 1
+
+## mainloop
+while game_running:
+  time.pause(pause_length) 
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Set up display
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("My Pygame")
+
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update game logic here
+
+    # Draw graphics here
+
+    # Refresh the display
+    pygame.display.flip()
+
